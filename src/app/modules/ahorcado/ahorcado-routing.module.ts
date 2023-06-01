f2a23bb500132474a8ceadf57f13a8415acf7503
Patricio <patodotaop@hotmail.com>
2023-06-01 15:08:02 -0300

    fix

--- src/app/modules/ahorcado/ahorcado-routing.module.ts
+++ src/app/modules/ahorcado/ahorcado-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AhorcadoComponent } from './ahorcado.component';
 
-const routes: Routes = [{ path: '', component: AhorcadoComponent }];
+const routes: Routes = [
+  { path: '', component:AhorcadoComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AhorcadoRoutingModule { }
